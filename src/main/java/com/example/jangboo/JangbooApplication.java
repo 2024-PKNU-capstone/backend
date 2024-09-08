32f9fcd88ed2cf247774a19a7454a83792be8229
onegqueen <rkdwldnjs30@naver.com>
2024-09-08 16:33:05 +0900

    feat: 스프링 프로젝트 생성 ref: #1

---
+++ src/main/java/com/example/jangboo/JangbooApplication.java
@@ -0,0 +1,13 @@
+package com.example.jangboo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JangbooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JangbooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jangboo/JangbooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jangboo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JangbooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
