b0007404435e7ce2ae16eb398ff617a95e8a216e
onegqueen <rkdwldnjs30@naver.com>
2024-09-27 16:15:41 +0900

    feat: jwt 토큰 및 security 처리 로직 클래스 추가 ref:#6

---
+++ src/main/java/com/example/jangboo/auth/JwtTokenProvider.java
@@ -0,0 +1,111 @@
+package com.example.jangboo.auth;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import com.example.jangboo.auth.controller.dto.Info.CurrentUserInfo;
+import com.example.jangboo.auth.controller.dto.response.JwtToken;
+import com.example.jangboo.auth.domain.CustomUserDetails;
+
+@Component
+public class JwtTokenProvider {
+	public static final String BEARER_TYPE = "Bearer";
+	public static final String AUTHORIZATION_HEADER = "Authorization";
+	public static final String REFRESH_HEADER = "Refresh";
+	public static final String BEARER_PREFIX = "Bearer ";
+
+	@Value("${jwt.secret_key}")
+	private String encodedSecretKey;
+
+	@Value("${jwt.access_token.valid_time}")
+	private Long accessTokenValidTime;
+
+	@Value("${jwt.refresh_token.valid_time}")
+	private Long refreshTokenValidTime;
+
+	public JwtToken generateTokenDto(CustomUserDetails customUserDetails) {
+		String accessToken = generateAccessToken(customUserDetails);
+		String refreshToken = generateRefreshToken(customUserDetails);
+		return new JwtToken(BEARER_TYPE,accessToken,refreshToken);
+	}
+
+	public CurrentUserInfo extractUserInfo(String token) {
+		Long userId = Long.parseLong(extractClaim(token, Claims::getSubject));
+		Long deptId = extractClaim(token, claims -> claims.get("deptId", Long.class));
+		return new CurrentUserInfo(userId, deptId);
+	}
+
+	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = extractAllClaims(token);
+		return claimsResolver.apply(claims);
+	}
+
+	private Claims extractAllClaims(String token) {
+		return Jwts.parser()
+			.setSigningKey(encodedSecretKey)
+			.parseClaimsJws(token)
+			.getBody();
+	}
+
+	public String generateAccessToken(CustomUserDetails userDetails) {
+		Map<String, Object> claims = new HashMap<>();
+
+		claims.put("deptId",userDetails.getDeptId());
+		return createToken(claims, userDetails.getUsername(),accessTokenValidTime);
+	}
+
+	public String generateRefreshToken(UserDetails userDetails) {
+		Map<String, Object> claims = new HashMap<>();
+		return createToken(claims, userDetails.getUsername(),refreshTokenValidTime);
+	}
+
+	public String createToken(Map<String, Object> claims, String userId, Long validTime) {
+		Date now = new Date();
+		Date validity = new Date(now.getTime() + validTime);
+
+		return Jwts.builder()
+			.setSubject(userId)
+			.setClaims(claims)
+			.setIssuedAt(now)
+			.setExpiration(validity)
+			.signWith(getSecretKey(), SignatureAlgorithm.HS256)
+			.compact();
+	}
+
+	private Key getSecretKey() {
+		byte[] keyBytes = Base64.getDecoder().decode(encodedSecretKey);
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
+
+	public boolean validateToken(String token) {
+		try {
+			Jwts.parserBuilder().setSigningKey(getSecretKey()).build().parseClaimsJws(token);
+			return true;
+		} catch (JwtException | IllegalArgumentException e) {
+			return false;
+		}
+	}
+
+	// 토큰 만료 여부 확인
+	private Boolean isTokenExpired(String token) {
+		return extractExpiration(token).before(new Date());
+	}
+
+	public Date extractExpiration(String token) {
+		return extractClaim(token, Claims::getExpiration);
+	}
+}
---
+++ src/main/java/com/example/jangboo/global/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,67 @@
+package com.example.jangboo.global.filter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.example.jangboo.auth.JwtTokenProvider;
+import com.example.jangboo.auth.controller.dto.Info.CurrentUserInfo;
+import com.example.jangboo.auth.domain.CustomUserDetails;
+import com.example.jangboo.auth.service.CustomUserDetailsService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+	private final CustomUserDetailsService userDetailsService;
+	private final JwtTokenProvider jwtTokenProvider;
+
+	private static final String HEADER_AUTHORIZATION = "Authorization";
+	private static final String TOKEN_PREFIX = "Bearer ";
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+		// 요청에서 Authorization 헤더를 추출하여 JWT 토큰 가져오기
+		final String authorizationHeader = request.getHeader(HEADER_AUTHORIZATION);
+
+		CurrentUserInfo userInfo = null;
+		String jwt = null;
+
+		// "Bearer "로 시작하는 헤더에서 토큰 추출
+		if (authorizationHeader != null && authorizationHeader.startsWith(TOKEN_PREFIX)) {
+			jwt = authorizationHeader.substring(7);
+			userInfo = jwtTokenProvider.extractUserInfo(jwt);
+		}
+
+		// 사용자가 인증되지 않았고, 토큰이 유효할 경우 인증 설정
+		if (userInfo != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+			UserDetails userDetails = this.userDetailsService.loadUserById(userInfo.userId());
+
+			// 토큰 유효성 검증
+			if (jwtTokenProvider.validateToken(jwt)) {
+				// 인증 객체 생성 및 설정
+				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+					new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+				usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+				// SecurityContext에 인증 설정
+				SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+			}
+		}
+
+		// 다음 필터로 요청을 전달
+		chain.doFilter(request, response);
+	}
+}
